8941def2154dfd42c077d38afb89d4c3601aa789
EKO <eofrpa@gmail.com>
2021-08-14 12:31:16 +0700

    ..

--- app/src/main/java/com/example/catatan/filedata.java
+++ app/src/main/java/com/example/catatan/filedata.java
@@ -1,14 +1,133 @@
 package com.example.catatan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class filedata extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class filedata extends AppCompatActivity implements View.OnClickListener {
+    public static final int REQUEST_CODE_STORAGE = 100;
+    int eventID = 0;
+    EditText editFileName, editContent;
+    Button btnSimpan;
+    String fileName = " ", tempCatatan = " ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filedata);
+
+        editFileName = findViewById(R.id.editFilename);
+        editContent = findViewById(R.id.editContent);
+        btnSimpan = findViewById(R.id.simpan);
+
+        btnSimpan.setOnClickListener(this);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            getSupportActionBar().setTitle("Ubah Catatan");
+            fileName = extras.getString("filename");
+            editFileName.setText(fileName);
+            editFileName.setEnabled(false);
+        } else {
+            getSupportActionBar().setTitle("Tambah Catatan");
+        }
+
+        eventID = 1;
+        if (Build.VERSION.SDK_INT >= 24) {
+            if (periksaIzinPenyimpanan()) {
+                bacaFile();
+            }
+        } else {
+            bacaFile();
+        }
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.simpan:
+                eventID = 2;
+                if (!tempCatatan.equals(editContent.getText().toString())) {
+                    if (Build.VERSION.SDK_INT >= 24) {
+                        if (periksaIzinPenyimpanan()) {
+                            tampilkanDialogKonfirmasiPenyimpanan();
+                        }
+                    } else {
+                        tampilkanDialogKonfirmasiPenyimpanan();
+                    }
+                } else {
+                    Toast.makeText(filedata.this, "Tidak ada perubahan yang dilakukan",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    private void tampilkanDialogKonfirmasiPenyimpanan() {
+    }
+
+    private void bacaFile() {
+        String path = getExternalFilesDir(null)+"/catatan";
+        File file = new File(path,editFileName.getText().toString());
+        if (file.exists()){
+            StringBuilder text = new StringBuilder();
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line = br.readLine();
+                while (line != null) {
+
+                    text.append(line);
+                    line=br.readLine();
+                }
+                br.close();
+            }catch (IOException e){
+                System.out.println("Error"+ e.getMessage());
+            }
+            tempCatatan=text.toString();
+            editContent.setText(text.toString());
+        }
+    }
+
+    private boolean periksaIzinPenyimpanan() {
+        if (Build.VERSION.SDK_INT >= 24) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                ;
+                return true;
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        REQUEST_CODE_STORAGE);
+                return false;
+            }
+        }else {
+            return false;
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,@NonNull int[] grantResult){
+        super.onRequestPermissionsResult(requestCode,permissions,grantResult);
+        switch (requestCode){
+            case REQUEST_CODE_STORAGE:
+                if (grantResult[0]==PackageManager.PERMISSION_GRANTED){
+                    bacaFile();
+                }else {
+                    tampilkanDialogKonfirmasiPenyimpanan();
+                }
+                break;
+        }
+    }
+
 }
--- app/src/main/java/com/example/catatan/menu.java
+++ app/src/main/java/com/example/catatan/menu.java
@@ -5,20 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 public class menu extends AppCompatActivity {
-    ListView item;
+    ListView listview;
     public static final int REQUEST_CODE_STORAGE = 100;
 
     @Override
@@ -27,21 +35,21 @@ public class menu extends AppCompatActivity {
         getSupportActionBar().setTitle("Catatan");
         setContentView(R.layout.activity_menu);
 
-        item = (ListView) findViewById(R.id.tampil);
-        item.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        listview = (ListView) findViewById(R.id.tampil);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener()
         {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id)
             {
                 Intent intent = new Intent(menu.this,filedata.class);
-                Map<String,Object> data = (Map<String, Object>)parent.getAdapter().getItem(position);
+                Map<String,Object> data = (Map<String,Object>)parent.getAdapter().getItem(position);
                 intent.putExtra("filename",data.get("name").toString());
                 Toast.makeText(menu.this, "You clicked"+data.get("name"), Toast.LENGTH_SHORT).show();
                 startActivity(intent);
             }
         });
 
-        item.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) 
             {
@@ -55,7 +63,7 @@ public class menu extends AppCompatActivity {
     @Override
     protected void onResume(){
         super.onResume();
-        if (Build.VERSION.SDK_INT>=23){
+        if (Build.VERSION.SDK_INT>=24){
             if (periksaIzinPenyimpanan()){
                 megambilListFilePadaFolder();
             }
@@ -63,9 +71,10 @@ public class menu extends AppCompatActivity {
             megambilListFilePadaFolder();
         }
     }
-
-    private boolean periksaIzinPenyimpanan() {
-        if(Build.VERSION.SDK_INT>=23){
+    private boolean periksaIzinPenyimpanan()
+    {
+        if(Build.VERSION.SDK_INT>=24)
+        {
             if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED){
                 return true;
             }else {
@@ -78,21 +87,83 @@ public class menu extends AppCompatActivity {
         }
     }
 
-    @Override
-    public void onRequestPermissionResult(int requestCode, @NonNull String[]permissions,@NonNull int[] grantResult){
+    public void onRequestPermissionResult(int requestCode, @NonNull String[] permissions,@NonNull int[] grantResult)
+    {
         super.onRequestPermissionsResult(requestCode, permissions,grantResult);
-        switch (requestCode){
+        switch (requestCode)
+        {
             case REQUEST_CODE_STORAGE:
-                if (grantResult[0]==PackageManager.PERMISSION_GRANTED){
+                if (grantResult[0]==PackageManager.PERMISSION_GRANTED)
+                {
                     megambilListFilePadaFolder();
                 }
                 break;
         }
     }
 
-    private void megambilListFilePadaFolder() {
+    void megambilListFilePadaFolder()
+    {
+        String path = getExternalFilesDir(null)+"/catatan";
+        File directory = new File(path);
+
+        if (directory.exists())
+        {
+            File[]files=directory.listFiles();
+            String[] filenames = new String[files.length];
+            String[] dateCreated = new String[files.length];
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MMM YYYY HH:mm:ss");
+            ArrayList<Map<String,Object>> itemDataList = new ArrayList<Map<String, Object>>();
+
+            for (int i=0;i <files.length; i++)
+            {
+                filenames[i] = files[i].getName();
+                Date lasModDate = new Date(files[i].lastModified());
+                dateCreated[i] = simpleDateFormat.format(lasModDate);
+
+                Map<String,Object> listitemMap = new HashMap<>();
+                listitemMap.put("name",filenames[i]);
+                listitemMap.put("date", dateCreated[i]);
+                itemDataList.add(listitemMap);
+            }
+
+            SimpleAdapter simpleAdapter = new SimpleAdapter(this,itemDataList, android.R.layout.simple_list_item_2,
+                    new String[]{"nama","date"},new int[]{android.R.id.text1,android.R.id.text2});
+            listview.setAdapter(simpleAdapter);
+            simpleAdapter.notifyDataSetChanged();
+        }
     }
 
-    private void tampilkanDialogKonfirmasiHapusCatatan(Object name, String toString) {
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        switch(item.getItemId()){
+            case R.id.Tambah:
+                Intent i = new Intent(this,filedata.class);
+                startActivity(i);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    void tampilkanDialogKonfirmasiHapusCatatan(Object name, String toString)
+    {
+        new AlertDialog.Builder(this).setTitle("Hapus Catatan ini ?")
+                .setMessage("Apakah Anda yakin ingin menghapus Catatan"+ filename +"?")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int which) {
+                        hapusFile(filename);
+                    }
+                }).setNegativeButton(android.R.string.no,null).show();
+    }
+
+    void hapusFile(String filename){
+        String path = getExternalFilesDir(null)+"/catatan";
+        File file = new File(path,filename);
+        if (file.exists()){
+            file.delete();
+        }
+        megambilListFilePadaFolder();
     }
 }
